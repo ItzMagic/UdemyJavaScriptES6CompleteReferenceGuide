cec84b7438ff894762e14c39aa943e869cf160bb
Evan Londono <esodemoneyes@gmail.com>
2019-01-17 16:12:09 -0500

    finishing the excerises for this module

---
+++ Excercise-Practice-1.js
@@ -0,0 +1,28 @@
+/*Exercise 1:
+
+Make use of the template literal to get the output of the below two variables as “Abhay is a programmer”
+
+var name = "Abhay"; //You can replace your name here.
+var vocation = "programmer"; //You can also replace what you do.
+console.log() //This is where you can write the code.
+*/
+var name = "Abhay";
+var vocation = "programmer";
+
+console.log(`${name} is a ${vocation}`);
+
+
+/*
+Exercise 2:
+
+You have two numbers, the first, is the original price, the second, is the discounted price.
+
+You need to work out what percentage a user saves if they purchase at the second price.
+
+Original price - 25, 
+New price - 10
+Output: in console.log()
+"You saved X% on this sale"  //You need to calculate the X
+//So, in this case your output will be:
+"You saved 60% on this sale
+*/
