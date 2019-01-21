9ac8c957068d279b50b4219cdcdc9d9b3c9f4fa8
Evan Londono <esodemoneyes@gmail.com>
2019-01-21 11:31:05 -0500

    first exercise for a for loop complete

---
+++ Exercises/Sec3-Lect12-Exercises/forLoops.js
@@ -0,0 +1,12 @@
+const dndRolls =['Barbarian', 'Bard', 'Cleric',
+'Druid',
+'Fighter',
+'Monk',
+'Paladin',
+'Ranger'];
+
+const countDown = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for(let i = 0; i < countDown.length; i++) {
+  num = countDown[i]
+  console.log(num)
+}
